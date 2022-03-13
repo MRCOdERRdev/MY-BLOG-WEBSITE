4fc8cea3d0f13541ce1f19b5bbee5c6c90b10df0
Tanmay <tanmay12ta@gmail.com>
2022-03-14 00:28:49 +0530

    "Host change"

--- main.py
+++ main.py
@@ -117,5 +117,5 @@ def new_post():
 
     return render_template('make-post.html',form=form,prop="New")
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', debug=True,port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False,port=5000)
+    # app.run(debug=True)
